793f54d39156a6eaa4de59693ab6190e17c8a6c4
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-20 11:12:28 +0900

    [Add] Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ function handleListening(){
     console.log(`Listening on : http://localhost:${PORT}`);
 }
 
-app.listen(PORT, handleListening);  //4000번 포트를 listening 시작하면 handleListening 함수 호출
+// ID나 PW를 보낸다면 req object로 받을 수 있음
+function handleHome(req, res){
+    res.send("Hello from home");    //요청에대한 응답을 보냄
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");  //응답으로 다음 문장을 보냄
+}
+
+// '/'은 main URL
+//main URL에 대한 요청(get)이 있으면 handleHome함수 호출
+app.get("/", handleHome);
+
+//main/profile에대한 요청에 있으면 handleProfile함수 홀출
+app.get("/profile", handleProfile);
+
+//4000번 포트를 listening 시작하면 handleListening 함수 호출
+app.listen(PORT, handleListening);  

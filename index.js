505539325d5ef31e163961b8b8a0deb401029a94
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-20 10:58:30 +0900

    [Add] My First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi~!');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);  //4000번 포트를 listening 시작하면 handleListening 함수 호출

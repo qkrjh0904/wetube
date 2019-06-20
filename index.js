26facd32460fef83a925bcda08568e0d9dfd3234
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-20 11:29:08 +0900

    [Add] ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const express = require('express');
+//const express = require('express'); 아래와 같이 변경
+import express from "express";
 const app = express();
 
 const PORT = 4000;
-
+/*
 function handleListening(){
     console.log(`Listening on : http://localhost:${PORT}`);
 }
+*/
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
 
 // ID나 PW를 보낸다면 req object로 받을 수 있음
+/*
 function handleHome(req, res){
     res.send("Hello from home");    //요청에대한 응답을 보냄
 }
+*/
+const handleHome = (req, res) => res.send("Hello from home.");
 
+/* 아래와 같이 변경 가능
 function handleProfile(req, res){
     res.send("You are on my profile");  //응답으로 다음 문장을 보냄
 }
+*/
+const handleProfile = (req, res) => res.send("You are on my profile.");
 
 // '/'은 main URL
 //main URL에 대한 요청(get)이 있으면 handleHome함수 호출

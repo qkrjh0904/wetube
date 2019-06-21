8e77c5adb25a3d705a16c8d16ab6357bf4226522
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-21 14:47:27 +0900

    [Add] MVC pattern -1

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-const app = express();
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
-const handleHome = (req, res) => res.send("Hello from home.");
-const handleProfile = (req, res) => res.send("You are on my profile.");
+const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -15,10 +15,8 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-
-
+app.use("/video", videoRouter);
 //누군가 내 파일을 immport할 때 app object를 주겠다는말임
 export default app; 

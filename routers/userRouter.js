8f437a6f4a0740fe354647def31bd32943ff0b39
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-21 17:31:01 +0900

    [Add] MVC pattern -3

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { users, userDetail, editProfile, ChangePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, ChangePassword);
 
 export default userRouter;
 

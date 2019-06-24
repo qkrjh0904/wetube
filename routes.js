96b8d46509525fd0f7cce5f0aed6c03b64789a5c
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-24 16:44:18 +0900

    [Add] Partials with Pug

--- routes.js
+++ routes.js
@@ -4,13 +4,11 @@ const JOIN = "/join";
 const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
-
 // Users
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
-
 // Videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";

7157491e64b16d73b92dc346841aecbb03ed39a4
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-24 17:19:02 +0900

    [Add] Templete Variables in Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,11 @@
 // Global Router
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
+//render의 첫 번째 인자는 templete이고, 두 번째 인자는 템플릿에 추가할 정보가 담긴 객체임
+export const home = (req, res) => res.render("home", { pageTitle : 'Home'});
+export const search = (req, res) => res.render("search", { pageTitle : 'Search'});
 
 // Video Router
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const videos = (req, res) => res.render("videos", { pageTitle : 'Videos'});
+export const upload = (req, res) => res.render("upload", { pageTitle : 'Upload'});
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : 'Video Detail'});
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle : 'Edit Video'});
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : 'Delete Video'});

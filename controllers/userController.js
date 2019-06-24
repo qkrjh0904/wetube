285fc157e7c465ec4583161c8a41df3bb05619ee
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-24 15:43:57 +0900

    [Add] Install Pug & [Refactor] send->render

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 // Global Router
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
 
 //User Router
 
-export const users =  (req, res) => res.send("Users");
-export const userDetail =  (req, res) => res.send("User Detail");
-export const editProfile =  (req, res) => res.send("Edit Profile");
-export const ChangePassword =  (req, res) => res.send("Change Password");
+export const users = (req, res) => res.render("users");
+export const userDetail = (req, res) => res.render("userDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const ChangePassword = (req, res) => res.render("changePassword");

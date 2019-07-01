b39c1189040cc5c56fd9ccea66150db571642b37
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-07-01 23:24:41 +0900

    [Add] Connecting to Mongo DB & Configuring Dot Env

--- db.js
+++ db.js
@@ -1,50 +1,24 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id: 111,
-        title: 'Video 111',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id : 12123,
-            name: "Jeongho",
-            email: "qkrwjdgh0904@naver.com"
-        }
-    },
-    {
-        id: 222,
-        title: 'Video 222',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id : 12123,
-            name: "Jeongho",
-            email: "qkrwjdgh0904@naver.com"
-        }
-    },
-    {
-        id: 333,
-        title: 'Video 333',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id : 12123,
-            name: "Jeongho",
-            email: "qkrwjdgh0904@naver.com"
-        }
-    },
-    {
-        id: 444,
-        title: 'Video 444',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id : 12123,
-            name: "Jeongho",
-            email: "qkrwjdgh0904@naver.com"
-        }
+        //내가 mongodb를 사용할 때 마다 이 configureation을 추가해 라는뜻(true)
+        //또는 이 configuration은 사용 안함으로해!라는 뜻(false)
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) => console.log("❌  Error on DB Connection");
+
+//한번만 실행됨
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+//dotenv를 설치한 이유는 가끔 내가 어떤 부분을 숨겨놓고 싶을 수 있기 때문

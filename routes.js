9fa2b3dd7bbc55577c285d4f1a21f09f930e8418
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-07-08 21:30:34 +0900

    [Add] Editing a Video

--- routes.js
+++ routes.js
@@ -42,7 +42,12 @@ const routes = {
         else
             return VIDEO_DETAIL;
     },
-    editVideo : EDIT_VIDEO,
+    editVideo : (id) =>{
+        if(id)
+            return `/videos/${id}/edit`;
+        else    
+            return EDIT_VIDEO;
+    },
     deleteVideo : DELETE_VIDEO
 };
 
